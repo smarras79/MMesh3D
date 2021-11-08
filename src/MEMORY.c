cd481b26aa723a5d103736e5419ecd535cb1821c
Simone <smarras@njit.edu>
2021-11-08 15:59:16 -0500

    Memory.c: replaced 1-allocation with 0-allocation

--- src/MEMORY.c
+++ src/MEMORY.c
@@ -38,15 +38,11 @@ int MEMORY_ALLOCATE(int flag)
       outfile_msh_gmsh  = (char*) malloc(96 * sizeof(char *));
       outfile_msh_alya = (char*) malloc(96 * sizeof(char *));
   
-      parameters = dvector(1,NUMBER_OF_PARAMETERS);
+      parameters = dvector(0,NUMBER_OF_PARAMETERS);
 
       //Allocation of the strings of the above lines:
       for(i=0; i<PROB_ENTRIES; i++)
 	problem[i] = (char *) malloc(32 * sizeof(char *));
-      
-      /*XGL and WGL*/
-      xgl = dvector(1,ngl);
-      wgl = dvector(1,ngl);
       
     }
   
@@ -54,13 +50,13 @@ int MEMORY_ALLOCATE(int flag)
     {
       printf(" EL_NODES %d\n", EL_NODES);
       /*coordinates*/
-      COORDS   = dmatrix(1,nnodes, 1,nsd+1);
-      COORDS1d = dvector(1,nnodes*(nsd+1));
+      COORDS   = dmatrix(0,nnodes,0,nsd+1);
+      COORDS1d = dvector(0,nnodes*(nsd+1));
       
       /*coonectivity*/
-      CONN = imatrix(1,nelem,1,EL_NODES+1); //For hexa only for now
-      for(iel=1; iel<=nelem; iel++){
-	for(inode=1; inode<=EL_NODES+1; inode++){
+      CONN = imatrix(0,nelem,0,EL_NODES+1); //For hexa only for now
+      for(iel=0; iel<=nelem; iel++){
+	for(inode=0; inode<=EL_NODES+1; inode++){
 	  CONN[iel][inode] = 0;
 	}
       }
@@ -69,14 +65,14 @@ int MEMORY_ALLOCATE(int flag)
       ELTYPE  = ivector(0,nelem+1);
   
       /*Bdy flag*/
-      BDYFLAG = dmatrix(1, nnodes, 1,nsd+1);
+      BDYFLAG = dmatrix(0, nnodes,0,nsd+1);
       
     }
 
   if(flag == 2)
     {     
       //Allocate space for the array of boundary coordinates to be read in input:
-      BDY_COORDS = dmatrix(1,NBDY_NODES, 1,nsd);
+      BDY_COORDS = dmatrix(0,NBDY_NODES,0,nsd);
     }
 
   if(flag == 3)
@@ -87,9 +83,9 @@ int MEMORY_ALLOCATE(int flag)
   if(flag == 4)
     {     
       //Allocate space for the array of boundary coordinates to be read in input:
-      x = d3tensor(1,nnodesx, 1,nnodesy, 1,nnodesz);
-      y = d3tensor(1,nnodesx, 1,nnodesy, 1,nnodesz);
-      z = d3tensor(1,nnodesx, 1,nnodesy, 1,nnodesz);
+      x = d3tensor(0,nnodesx,0,nnodesy,0,nnodesz);
+      y = d3tensor(0,nnodesx,0,nnodesy,0,nnodesz);
+      z = d3tensor(0,nnodesx,0,nnodesy,0,nnodesz);
     }
   
   return 0;
@@ -114,20 +110,16 @@ int MEMORY_DEALLOCATE(int flag)
   
       for(i=0; i<PROB_ENTRIES; i++)
 	free(problem[i]);
-
-      
-      free_dvector(xgl, 1,ngl);
-      free_dvector(wgl, 1,ngl);
     }
   if(flag == 1)
     {
       
-      free_dvector(parameters,1,NUMBER_OF_PARAMETERS);
-      free_dmatrix(COORDS,   1,nnodes, 1,nsd+1);
-      free_dvector(COORDS1d, 1,nnodes*(nsd+1));
-      free_imatrix(CONN,   1,nelem,1,EL_NODES+1);
-      free_ivector(ELTYPE, 0,nelem+1);
-      free_dmatrix(BDY_COORDS, 1,NBDY_NODES, 1,nsd);
+      free_dvector(parameters,0,NUMBER_OF_PARAMETERS);
+      free_dvector(COORDS1d,  0,nnodes*(nsd+1));
+      free_ivector(ELTYPE,    0,nelem+1);
+      free_dmatrix(COORDS,    0,nnodes,     0,nsd+1);
+      free_imatrix(CONN,      0,nelem,      0,EL_NODES+1);
+      free_dmatrix(BDY_COORDS,0,NBDY_NODES, 0,nsd);
     }
   
   if(flag == 3)
@@ -136,9 +128,9 @@ int MEMORY_DEALLOCATE(int flag)
   if(flag == 4)
     {     
       //Allocate space for the array of boundary coordinates to be read in input:
-      free_d3tensor(x, 1,nnodesx, 1,nnodesy, 1,nnodesz);
-      free_d3tensor(y, 1,nnodesx, 1,nnodesy, 1,nnodesz);
-      free_d3tensor(z, 1,nnodesx, 1,nnodesy, 1,nnodesz);
+      free_d3tensor(x,0,nnodesx,0,nnodesy,0,nnodesz);
+      free_d3tensor(y,0,nnodesx,0,nnodesy,0,nnodesz);
+      free_d3tensor(z,0,nnodesx,0,nnodesy,0,nnodesz);
     }
 
 return 0;    
